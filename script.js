15430df176cbfa8b92ebf62f89585225dbe66be6
Kasie97 <66069819+Kasie97@users.noreply.github.com>
2023-08-23 12:36:39 +0100

    Updated Star Wars API

--- script.js
+++ script.js
@@ -1,60 +1,46 @@
-const images = ['Skyw.png', 'C-3PO2.png', 'R2-D2.png', 'Darth-Vader.png', 'Leia.png', 'Owen-Lars.png', 'Beru-Whitesun-Lars.png', 'R5-D4.png', 'Biggs-Darklighter.png', 'Obi-Wan-Kenobi.png'];
+const images = ['Skywalker.png', 'C-3PO2.png', 'R2-D2.png', 'Darth-Vader.png', 'Leia-Organa.png', 'Owen-Lars.png', 'Beru-Whitesun-Lars.png', 'R5-D4.png', 'Biggs-Darklighter.png', 'Obi-Wan-Kenobi.png'];
 
 
-let mainContainer = document.getElementById('container')
-let result = []
-let datadetail = '';
+async function fetchPeople() {
+    const response = await fetch("https://swapi.dev/api/people");
+    const data = await response.json();
+    return data.results;
+  }
 
-async function getPeople() {
-    let result = await fetch("https://swapi.dev/api/people")
-    let data = await result.json()
-    results = data.results
-      let array = data.results
-    array.forEach((item, index) => {
+  async function renderCards() {
+    const people = await fetchPeople();
+    const cardRow = document.getElementById("cardRow");
+    const modalBody = document.getElementById("modalBody");
 
-       let card = `
-       <div class="card" style="width: 20rem;">
-           <img src=${images[index]} class="card-img-top" alt="...">
-           <div class="card-body">
-                <h5 class="card-title">${item.name}</h5>
-                <a href="#" class="btn btn-dark" data-toggle="modal" data-target="#myModal">More Info</a>
-            </div>
-        </div>`
+    people.forEach(person => {
+      const card = document.createElement("div");
+      card.classList.add("col-md-2", "mb-4");
+      card.innerHTML = `
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">${person.name}</h5>
+            <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#infoModal" data-info="${JSON.stringify(person, null, 2)}">
+              More Info
+            </button>
+          </div>
+        </div>
+      `;
+      cardRow.appendChild(card);
+    });
 
-    datadetail += card
+    // Show more info in the modal
+    const modal = new bootstrap.Modal(document.getElementById("infoModal"));
+    cardRow.addEventListener("click", event => {
+      const target = event.target;
+      if (target.classList.contains("btn")) {
+        const info = JSON.parse(target.getAttribute("data-info"));
+        modalBody.innerHTML = `
+          <p>Height: ${person.height}</p>
+          <p>Gender: ${person.gender}</p>
+        `;
+        modal.show();
+      }
+    });
+  }
 
-    mainContainer.innerHTML += card;
-    });     
-}
-getPeople()
-
-document.getElementById("character-detail").addEventListener("click",moreInfo)
-
-function moreInfo(e){
-    const clickedActor = e.target
-    if(clickedActor.classlist.contains ('name')){
-        let actor = e.target.parentElement.querySelector(".name")
-        console.log(actor)
-
-        let drop = star.nextElementSibling
-    }
-}
-
-// //document.getElementById('btn').addEventListener("click", moreInfo)
-// document.addEventListener("DOMContentLoaded", function() {
-//     const openModalButton = document.getElementById("openModalButton");
-//     const modalName = document.getElementById("modalName");
-//     const modalGender = document.getElementById("modalGender");
-//     const modalHeight = document.getElementById("modalHeight");
-
-//     openModalButton.addEventListener("click", function() {
-//         // Update modal content here
-//         modalName.textContent = "John Doe";
-//         modalGender.textContent = "Gender: Male";
-//         modalHeight.textContent = "Height: 180 cm";
-
-//         // Show the modal
-//         const modal = new bootstrap.Modal(document.getElementById("exampleModal"));
-//         modal.show();
-//     });
-// });
+  renderCards();
